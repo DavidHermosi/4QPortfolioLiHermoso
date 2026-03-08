93df663d6452b833e6f6c05ec86b29388f3b2c1a
DavidHermosi <b2029dibhermoso@pshs.edu.ph>
2026-03-08 18:03:31 +0800

    html

--- public/MR.js
+++ public/MR.js
@@ -1,8 +1,3 @@
 let StarRating = 0;
 
 const  stars = document.querySelectorAll('.star');
-const movieList = document.querySelector('.movie-list');
-
-stars.forEach((star, index) => {
-    star.addEventListener('click', () => {
-        selectedRating=parse
