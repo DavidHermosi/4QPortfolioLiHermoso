80050e8d577d49302e8133fbc1c57e756f17fff7
DavidHermosi <b2029dibhermoso@pshs.edu.ph>
2026-03-08 18:52:31 +0800

    js and css

--- public/MR.js
+++ public/MR.js
@@ -1,3 +1,71 @@
 let StarRating = 0;
 
 const  stars = document.querySelectorAll('.star');
+const movieList = document.getElementById('movieList');
+
+
+stars.forEach((star, index) => {
+  star.addEventListener('click', () => {
+    StarRating = index + 1;
+    updateStars();
+  });
+
+  star.addEventListener('mouseover', () => {
+    updateStars(index + 1);
+  });
+
+  star.addEventListener('mouseout', () => {
+    updateStars();
+  });
+
+});
+
+function updateStars(rating = StarRating) {
+  stars.forEach((star, index) => {
+    if (index < rating) {
+      star.classList.add('filled');
+    } else {
+      star.classList.remove('filled');
+    }
+  });
+}
+
+function getMovies() {
+  const movies = JSON.parse(localStorage.getItem('movies')) || [];
+  return movies;
+}
+
+function addMovie(title, year, genre, rating) {
+  const movies = getMovies();
+  movies.push({ title, year, genre, rating });
+  localStorage.setItem('movies', JSON.stringify(movies));
+}
+
+function displayMovies() {
+  const movies = getMovies();
+  movieList.innerHTML = '';
+    movies.forEach(movie => {
+    const li = document.createElement('li');
+    li.innerHTML = `${movie.title} (${movie.year}) - ${movie.genre}, Rating: <span style="color: #f59e0b">${'★'.repeat(parseInt(movie.rating))}</span>`;
+    movieList.appendChild(li);
+  }
+
+    );
+}
+
+document.getElementById('movieForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const title = document.getElementById('movieTitle').value;
+    const year = document.getElementById('movieYear').value;
+    const genre = document.getElementById('movieGenre').value;
+    addMovie(title, year, genre, StarRating);
+    displayMovies();
+    this.reset();
+    StarRating = 0;
+    updateStars();
+});
+
+displayMovies();
+
+
+
